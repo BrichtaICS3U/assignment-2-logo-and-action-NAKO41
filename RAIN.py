16323c7be6e9b147c100af955e1bb544a1211d9e
NSPro4w <nspro1@ocdsb.ca>
2018-04-12 13:08:13 -0400

    rain has been added

---
+++ RAIN.py
@@ -0,0 +1,23 @@
+import pygame, random
+RainBlue = (3,74,236)
+WHITE = (255,255,255)
+ 
+
+class RAIN(pygame.sprite.Sprite):
+
+
+   def __init__ (self,  color, width = 2, height = 2, x = random.randint(2,398)):
+       super().__init__()
+
+
+       self.image = pygame.Surface([width,height])
+       self.image.fill(WHITE)
+       self.image.set_colorkey(WHITE)
+
+
+       pygame.draw.ellipse(self.image,color,[x,0,width,height])
+
+       sefl.ellipse = self.image.get_ellipse()
+       
+
+    
