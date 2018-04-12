178a2e80fd108f7d06ade12ca7f0f11c5b645e72
NSPro4w <nspro1@ocdsb.ca>
2018-04-12 13:14:19 -0400

    rain

--- RAIN.py
+++ RAIN.py
@@ -6,7 +6,7 @@ WHITE = (255,255,255)
 class RAIN(pygame.sprite.Sprite):
 
 
-   def __init__ (self,  color = RainBlue, width = 2, height = 2, x = random.randint(2,398)):
+   def __init__ (self,  color = RainBlue, width = 2, height = 2, x = random.randint(2,398) , y = 0):
        super().__init__()
 
 
@@ -15,9 +15,13 @@ class RAIN(pygame.sprite.Sprite):
        self.image.set_colorkey(WHITE)
 
 
-       pygame.draw.ellipse(self.image,color,[x,0,width,height])
+       pygame.draw.ellipse(self.image,color,[x,y,width,height])
 
        self.ellipse = self.image.get_ellipse()
+
+
+    def fall(self,pixels):
+        self.ellipse.y += 1
        
 
     
